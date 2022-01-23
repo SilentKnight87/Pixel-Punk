42e8d2683ba800690505485208710fd8e552486b
Peter B <petrojoash@yahoo.com>
2022-01-23 17:07:26 -0500

    First commit, added header & Punk Logo

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css"
+import punkLogo from "../assets/header/cryptopunk-logo.png"
+
+const Header = () => {
+  return <div className='header'>
+       <div className='logoContainer'>
+         <img  src={punkLogo} className='punkLogo' alt=''/>
+                   </div>
+       </div>
+  
+};
+
+export default Header;
