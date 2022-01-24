0e3bfdfec45a0fb5ca299844c68b407602f0a417
Peter B <petrojoash@yahoo.com>
2022-01-24 12:04:27 -0500

    Added CollectionCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
-  return ( <div className='app'><Header /></div>
-   
+  return (
+    <div className="app">
+      <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image={
+          "https://ipfs.thirdweb.com/ipfs/QmWNsgfxfqtQDArEwWxa4C71ip77PFNy9rtdUqoqiyxVw3/0.jpg"
+        }
+      />
+    </div>
   );
 }
 
