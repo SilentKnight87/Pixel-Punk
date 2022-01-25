53b35a86b415a6e2580d06a960c4d67dc60d7c2c
Peter B <petrojoash@yahoo.com>
2022-01-24 21:07:48 -0500

    Added collection cards, and opensea api

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xAF5D33b502cB8dc3fC623b5ac73D97B961e45473&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image={
-          "https://ipfs.thirdweb.com/ipfs/QmWNsgfxfqtQDArEwWxa4C71ip77PFNy9rtdUqoqiyxVw3/0.jpg"
-        }
-      />
+
+      <Punklist punkListData={punkListData} />
     </div>
   );
 }
